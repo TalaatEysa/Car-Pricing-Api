f4a393c1f130f7ade17a4ce2163a43f8954413ec
TalaatEysa <talaateysa@outlook.com>
2024-12-31 12:56:14 +0200

    implement custom data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Post, Body, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    Get,
+    Patch,
+    Param,
+    Query,
+    Delete,
+    NotFoundException,
+    UseInterceptors
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
     @Post('/signup')
@@ -11,7 +25,6 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
@@ -30,11 +43,9 @@ export class UsersController {
     removeUser(@Param('id') id: string) {
         return this.usersService.remove(parseInt(id));
     }
-    
+
     @Patch('/:id')
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
         return this.usersService.update(parseInt(id), body);
-        
     }
-
 }
