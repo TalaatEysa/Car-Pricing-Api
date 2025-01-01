7351e7ea0e38664cb702939283f1d2895b3607fd
TalaatEysa <talaateysa@outlook.com>
2025-01-01 15:23:31 +0200

    implementing signin functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -22,10 +22,16 @@ export class UsersController {
     constructor(
         private usersService: UsersService,
         private authService: AuthService
-    ) {}
+    ) { }
+    
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         return this.authService.signup(body.email, body.password);
+    }
+
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);
     }
 
     @Get('/:id')
