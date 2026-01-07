ec48dc04b8e4cd5133be6ca135295f621d4dc66a
TalaatEysa <talaateysa@outlook.com>
2026-01-07 22:20:03 +0200

    implement auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,16 +8,18 @@ import {
     Query,
     Delete,
     NotFoundException,
-    Session
+    Session,
+    UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,8 +28,8 @@ export class UsersController {
         private usersService: UsersService,
         private authService: AuthService
     ) { }
-    
     @Get('/whoami')
+    @UseGuards(AuthGuard)
     whoami(@CurrentUser() user: User) {
         return user;
     }
