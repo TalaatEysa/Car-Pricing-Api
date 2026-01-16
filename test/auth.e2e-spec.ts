c42468be805aa41fea0c17e28d734b699446481e
TalaatEysa <talaateysa@outlook.com>
2026-01-16 17:51:17 +0200

    setup app configuration

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
     });
 
     it('handles a signup request', () => {
-        const email = 'tala7@test.com';
+        const email = 'talaat@test.com';
         return request(app.getHttpServer())
             .post('/auth/signup')
             .send({ email, password: 'testpassword' })
@@ -26,5 +26,21 @@ describe('Authentication System', () => {
                 expect(id).toBeDefined();
                 expect(email).toEqual(email);
             });
+    });
+
+    it('signup as a new user then get the currently logged in user', async () => {
+        const email = 'talaat@test.com';
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({ email, password: 'testpassword' })
+            .expect(201);
+
+        const cookie = res.get('Set-Cookie');
+        const { body } = await request(app.getHttpServer())
+            .get('/auth/whoami')
+            .set('Cookie', cookie)
+            .expect(200);
+
+        expect(body.email).toEqual(email);
     });
 });
