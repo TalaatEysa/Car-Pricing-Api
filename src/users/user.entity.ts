05356bbd3966a1a09e01989c292069aa1d2be576
TalaatEysa <talaateysa@outlook.com>
2026-01-17 02:05:20 +0200

    feat:implement create report by user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate, OneToMany } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -10,6 +11,9 @@ export class User {
 
     @Column()
     password: string;
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[]
 
     @AfterInsert()
     logInsert() {
@@ -25,5 +29,5 @@ export class User {
     logRemove() {
         console.log(`Removed user with id ${this.id}`);
     }
-    
+
 }
